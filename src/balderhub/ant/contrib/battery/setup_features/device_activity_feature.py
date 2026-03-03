d34ce03b551d4a109d37bc258b659ff24beb99cd
matosys <max@matosys.de>
2026-03-03 16:47:11 +0100

    feat(contrib/battery): move channel creation/deletion for `DeviceActivityFeature` into prepare/cleanup methods

--- src/balderhub/ant/contrib/battery/setup_features/device_activity_feature.py
+++ src/balderhub/ant/contrib/battery/setup_features/device_activity_feature.py
@@ -7,11 +7,33 @@ class DeviceActivityFeature(balderhub.battery.lib.scenario_features.DeviceActivi
 
     ant_controller = AntplusControllerHrmFeature()
 
-    def is_active(self) -> bool:
-        if not self.ant_controller.channel_is_active:
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self._channel_was_active_before = None
+
+    @property
+    def time_to_wait_for_new_msg_sec(self):
+        """
+        :return: returns the time in seconds, the feature should wait for a new ant broadcast message
+        """
+        return 3
+
+    def prepare(self) -> None:
+        self._channel_was_active_before = self.ant_controller.channel_is_active
+        if not self._channel_was_active_before:
             self.ant_controller.open_channel()
+
+    def cleanup(self) -> None:
+        if not self._channel_was_active_before:
+            self.ant_controller.close_channel()
+            self._channel_was_active_before = None
+
+    def is_active(self) -> bool:
+        if self._channel_was_active_before is not None:
+            raise ValueError('can not check if ANT is active because this call was not embedded within prepare/cleanup '
+                             'calls')
         try:
-            self.ant_controller.wait_for_new_broadcast_message(timeout=1)
+            self.ant_controller.wait_for_new_broadcast_message(timeout=self.time_to_wait_for_new_msg_sec)
             return True
         except TimeoutError:
             return False
