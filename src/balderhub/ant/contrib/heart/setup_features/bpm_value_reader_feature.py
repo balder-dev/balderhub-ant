fb7aab48b9302cdc89b1b1b0e4e1c0b0c617e6c4
matosys <max@matosys.de>
2026-03-03 16:48:42 +0100

    feat(contrib/heart): move channel creation/deletion for `BpmValueReaderFeature` into prepare/cleanup methods

--- src/balderhub/ant/contrib/heart/setup_features/bpm_value_reader_feature.py
+++ src/balderhub/ant/contrib/heart/setup_features/bpm_value_reader_feature.py
@@ -9,7 +9,31 @@ class BpmValueReaderFeature(balderhub.heart.lib.scenario_features.BpmValueReader
     """
     ant_controller = AntplusControllerHrmFeature()
 
-    def read_last_bpm_value(self):
-        if not self.ant_controller.channel_is_active:
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+        self._channel_was_active_before = None
+
+    @property
+    def time_to_wait_for_new_msg_sec(self) -> float:
+        """
+        :return: returns the time in seconds, the feature should wait for a new ant broadcast message
+        """
+        return 1.0
+
+    def prepare(self):
+        self._channel_was_active_before = self.ant_controller.channel_is_active
+        if not self._channel_was_active_before:
             self.ant_controller.open_channel()
-        return self.ant_controller.wait_for_new_broadcast_message(timeout=1).computed_heart_rate
+
+    def read_last_bpm_value(self):
+        if self._channel_was_active_before is not None:
+            raise ValueError('can not check if ANT is active because this call was not embedded within prepare/cleanup '
+                             'calls')
+        new_msg = self.ant_controller.wait_for_new_broadcast_message(timeout=self.time_to_wait_for_new_msg_sec)
+        return new_msg.computed_heart_rate
+
+    def cleanup(self):
+        if not self._channel_was_active_before:
+            self.ant_controller.close_channel()
+            self._channel_was_active_before = None
