b3c5c535d3fed191db0bd4625cd9619bb98f64f7
matosys <max@matosys.de>
2026-03-05 16:55:48 +0100

    fix(contrib/heart): add implementation for method `RRValueReaderFeature.wait_for_next_rr_value_in_sec`

--- src/balderhub/ant/contrib/heart/setup_features/rr_value_reader_feature.py
+++ src/balderhub/ant/contrib/heart/setup_features/rr_value_reader_feature.py
@@ -1,7 +1,9 @@
+from typing import Union
 import time
 
 import balderhub.heart.lib.scenario_features
 from balderhub.ant.lib.scenario_features import AntplusControllerHrmFeature
+from balderhub.ant.lib.utils.pages.hrm import BaseHrmPage
 
 
 class RRValueReaderFeature(balderhub.heart.lib.scenario_features.RRValueReaderFeature):
@@ -21,42 +23,84 @@ class RRValueReaderFeature(balderhub.heart.lib.scenario_features.RRValueReaderFe
         """
         :return: returns the time in seconds, the feature should wait for a new ant broadcast message
         """
-        return 3.0
+        return 10.0  # TODO set lower
 
     def prepare(self):
         self._channel_was_active_before = self.ant_controller.channel_is_active
         if not self._channel_was_active_before:
             self.ant_controller.open_channel()
 
-    def read_last_rr_value_in_sec(self) -> float:
-        # TODO optimize
+    def __check_for_channel(self):
         if self._channel_was_active_before is not None:
             raise ValueError('can not check if ANT is active because this call was not embedded within prepare/cleanup '
                              'calls')
 
+    def _msg_has_next_beat(self, msg: BaseHrmPage, msg_before: BaseHrmPage):
+        if msg.heart_beat_count == 0:
+            return msg_before.heart_beat_count == 255
+        return msg_before.heart_beat_count == (msg.heart_beat_count - 1)
+
+    def _calc_rr_value_for(self, msg: BaseHrmPage, msg_before: BaseHrmPage) -> float:
+        if not self._msg_has_next_beat(msg, msg_before):
+            raise ValueError('can not calculate RR value because previous message has not the expected heart '
+                             f'beat count of {msg_before.heart_beat_count} (current has {msg.heart_beat_count})')
+        cur_msg_event_time = msg.heart_beat_event_time
+        cur_msg_event_time += 0xFFFF if cur_msg_event_time < msg_before.heart_beat_event_time else 0
+
+        return (cur_msg_event_time - msg_before.heart_beat_event_time) / 1024
+
+    def wait_for_next_rr_value_in_sec(self) -> Union[float, None]:
+        self.__check_for_channel()
+        # first wait for first message
         start_time = time.perf_counter()
         while time.perf_counter() - start_time < self.time_to_wait_for_new_msg_sec:
-            cur_msg = self.ant_controller.wait_for_new_broadcast_message(timeout=1)
+            if len(self.ant_controller.received_broadcast_messages) > 0:
+                # one or more messages here -> break
+                break
+            time.sleep(self.time_to_wait_for_new_msg_sec / 100)
+        else:
+            return None
+        last_rcvd_msg = self.ant_controller.received_broadcast_messages[-1]
+
+        # now wait for the next one
+        while time.perf_counter() - start_time < self.time_to_wait_for_new_msg_sec:
+            newest_msg = self.ant_controller.received_broadcast_messages[-1]
+            if newest_msg.heart_beat_count != last_rcvd_msg.heart_beat_count:
+                # one or more messages here -> break
+                if not self._msg_has_next_beat(newest_msg, last_rcvd_msg):
+                    raise ValueError(
+                        'detect unexpected heart beats for two messages - received new message with heart beat count '
+                        f'of {newest_msg.heart_beat_count} (msg before was {last_rcvd_msg.heart_beat_count})'
+                    )
+                return self._calc_rr_value_for(newest_msg, last_rcvd_msg)
+            time.sleep(self.time_to_wait_for_new_msg_sec / 100)
+        return None
+
+    def read_last_rr_value_in_sec(self) -> Union[float, None]:
+        self.__check_for_channel()
+
+        start_time = time.perf_counter()
+        while time.perf_counter() - start_time < self.time_to_wait_for_new_msg_sec:
             if len(set(msg.heart_beat_count for msg in self.ant_controller.received_broadcast_messages)) > 1:
                 # more than one messages here -> break
                 break
+            time.sleep(self.time_to_wait_for_new_msg_sec / 100)
         else:
-            raise TimeoutError(f'did not received more than one heart beat messages within '
-                               f'{self.time_to_wait_for_new_msg_sec} seconds')
+            return None
 
-        heart_beat_count_before = cur_msg.heart_beat_count
+        messages = self.ant_controller.received_broadcast_messages
+        last_msg = messages[-1]
+        heart_beat_count_before = (last_msg.heart_beat_count - 1) % 0x100
 
-        all_msgs = reversed(self.ant_controller.received_broadcast_messages.messages)
+        all_msgs = reversed(messages)
         for msg in all_msgs:
-            if msg.heart_beat_count not in [heart_beat_count_before, cur_msg.heart_beat_count]:
+            if msg.heart_beat_count not in [heart_beat_count_before, last_msg.heart_beat_count]:
                 raise ValueError(f'detect message with unexpected heart beat count of {msg.heart_beat_count} '
-                                 f'(current message is {cur_msg.heart_beat_count})')
+                                 f'(current message is {last_msg.heart_beat_count})')
             if msg.heart_beat_count == heart_beat_count_before:
-                cur_msg_event_time = cur_msg.heart_beat_event_time
-                cur_msg_event_time += 0xFFFF if cur_msg_event_time < msg.heart_beat_event_time else 0
-
-                return (msg.heart_beat_event_time - cur_msg_event_time) / 1024
-        raise ValueError('was unable to detect a correct previous heart beat message')
+                # get time of message with heart beat before
+                return self._calc_rr_value_for(last_msg, msg)
+        return None
 
     def cleanup(self):
         if not self._channel_was_active_before:
